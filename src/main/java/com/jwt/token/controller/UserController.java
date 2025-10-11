ca5581bee138dd69bb3a9ab97bb1dcccab54ec3e
KD1_70106_YogeshPatil <yogeshpatil2211999@gmail.com>
2025-10-11 15:14:16 +0530

    swagger - implementation

--- src/main/java/com/jwt/token/controller/UserController.java
+++ src/main/java/com/jwt/token/controller/UserController.java
@@ -2,6 +2,8 @@ package com.jwt.token.controller;
 
 import com.jwt.token.entity.Users;
 import com.jwt.token.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Tag(name = "User Management", description = "API's for managing users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "User Login", description = "Login for user")
     @PostMapping("/login")
     public String login(@RequestBody Users users){
         System.out.println("user: "  + users.toString());
